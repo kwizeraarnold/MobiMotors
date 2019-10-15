3675fc67cd9b2e17d895dc8bc6539d906d1542b9
kwizeraarnold <nicokwizera@gmail.com>
2019-10-15 16:55:05 +0300

    initial project setup

---
+++ app/src/main/java/com/mobi/mobimotors/MainActivity.java
@@ -0,0 +1,45 @@
+package com.mobi.mobimotors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.chivorn.smartmaterialspinner.SmartMaterialSpinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private SmartMaterialSpinner spProvince;
+    private List<String> provinceList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        initSpinner();
+    }
+    private  void initSpinner(){
+        spProvince = findViewById(R.id.spinner1);
+        provinceList = new ArrayList<>();
+        provinceList.add("Kwizera");
+        provinceList.add("Nicholas");
+        provinceList.add("Arnold");
+        spProvince.setItem(provinceList);
+        spProvince.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,provinceList.get(position),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
