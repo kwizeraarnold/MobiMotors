60cbe2e726ba8c0793238adddaaef50f7ee7ba79
KamasuPaul <kamasupaul@gmail.com>
2019-10-16 18:11:27 +0300

    added filters fragment and removed some unwanted space

--- app/src/main/java/com/mobi/mobimotors/ui/favourate/FavouratesFragment.java
+++ app/src/main/java/com/mobi/mobimotors/ui/favourate/FavouratesFragment.java
@@ -22,14 +22,14 @@ public class FavouratesFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(FavouratesViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        View root = inflater.inflate(R.layout.activity_search, container, false);
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        notificationsViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
