6c453a76bc9c92ec040d37edc9d4dc7f54cc21bc
KamasuPaul <kamasupaul@gmail.com>
2019-11-07 19:33:00 +0300

    added double sided seekbar and some of its functionality

--- app/src/main/java/com/mobi/mobimotors/ActivityHelper.java
+++ app/src/main/java/com/mobi/mobimotors/ActivityHelper.java
@@ -1,6 +1,7 @@
 package com.mobi.mobimotors;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -9,21 +10,25 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class ActivityHelper extends AppCompatActivity implements StepsFragment.OnFragmentInteractionListener {
-    StepsFragment stepsFragment;
+    public static StepsFragment stepsFragment;
     FragmentTransaction fragmentTransaction;
-    PriceFragment priceFragment;
+   public  static PriceFragment priceFragment;
+   public static FinalStepFragment finalStepFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         stepsFragment = new StepsFragment();
         priceFragment = new PriceFragment();
+        finalStepFragment = new FinalStepFragment();
 
         setContentView(R.layout.activity_helper);
         fragmentTransaction = getSupportFragmentManager().beginTransaction();
         Bundle bundle = getIntent().getExtras();
         String chosenCategory = bundle.getString("category");
         Toast.makeText(this,chosenCategory,Toast.LENGTH_SHORT).show();
+        //send the data in the bundle to the fragment
+        stepsFragment.setArguments(bundle);
 
         fragmentTransaction.replace(R.id.fragment_root,stepsFragment);
         fragmentTransaction.commit();
@@ -33,9 +38,9 @@ public class ActivityHelper extends AppCompatActivity implements StepsFragment.O
     public void onFragmentInteraction(Uri uri) {
 
     }
-    public void gotoNextFragment(){
+    public void gotoNextFragment(Fragment fragment){
         fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_root,priceFragment);
+        fragmentTransaction.replace(R.id.fragment_root,fragment);
         fragmentTransaction.commit();
     }
 }
