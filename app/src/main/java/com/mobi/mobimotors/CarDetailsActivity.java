377ee37e954b357b4c15a566849027f767edb626
kwizeraarnold <nicokwizera@gmail.com>
2019-11-09 09:50:46 +0300

    car details added

---
+++ app/src/main/java/com/mobi/mobimotors/CarDetailsActivity.java
@@ -0,0 +1,18 @@
+package com.mobi.mobimotors;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class CarDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_details);
+    }
+}
